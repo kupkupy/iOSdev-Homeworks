647d67113eaacca7e7f0e6405fab7d29f28f0bc0
Pavlova Tatyana <etaniks@mail.ru>
2022-04-10 21:26:49 +0300

    No changes

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -38,19 +38,16 @@ class FeedViewController: UIViewController {
         
         let buttons = [firstButton, secondButton]
         buttons.enumerated().forEach({ (index, button) in
+            button.layer.cornerRadius = 12
+            button.setTitleColor(.white, for: .normal)
+            button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
             switch index {
             case 0:
                 button.setTitle("Open post", for: .normal)
-                button.layer.cornerRadius = 12
                 button.backgroundColor = .systemYellow
-                button.setTitleColor(.white, for: .normal)
-                button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
             case 1:
                 button.setTitle("Read more", for: .normal)
-                button.layer.cornerRadius = 12
                 button.backgroundColor = .systemPurple
-                button.setTitleColor(.white, for: .normal)
-                button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
             default:
                 break
             }
--- Navigation/Post/PostViewController.swift
+++ Navigation/Post/PostViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class PostViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .systemMint
-    }
+            super.viewDidLoad()
+            self.view.backgroundColor = .systemMint
+        }
+    
 }
